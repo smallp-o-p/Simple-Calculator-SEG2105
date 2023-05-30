c34025e9724965328fe86450d13e421a55de5175
William Tran-Viet <wtran013@uottawa.ca>
2023-05-29 20:40:06 -0400

    a

--- app/src/main/java/com/example/simple_calculator/MainActivity.kt
+++ app/src/main/java/com/example/simple_calculator/MainActivity.kt
@@ -25,17 +25,7 @@ class MainActivity : ComponentActivity() {
     private var optr: Operator = Operator.none
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SimpleCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.layout1)
     }
 
     public fun btn00click(view: View) : Unit {
